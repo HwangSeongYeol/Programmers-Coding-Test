866efb2a3518883325fc1a70973e20b6a83993b7
황성열 <clclcl789@naver.com>
2019-12-10 01:01:29 +0900

    아스키 순서가 애초에 ABCD~~abcd순이라 역순으로 정렬하면 됨

--- Level 1/문자열 내림차순으로 배치하기.cpp
+++ Level 1/문자열 내림차순으로 배치하기.cpp
@@ -4,29 +4,8 @@
 
 using namespace std;
 
-bool cmp(char a, char b) {
-    if(a < 'A' && b < 'A') {
-        return a > b;
-    }
-    else if(a < 'A' && b >= 'A') {
-        return a < b;
-    }
-    else if(a >= 'A' && b < 'A') {
-        return a > b;
-    }
-    else
-        return a > b;
+string solution(string s) {
+    sort(s.begin(), s.end(), greater<char>());
+    return s;
 }
 
-string solution(string s) {
-    string answer = "";
-    vector<char> STRING;
-    for(int i = 0; i < s.length(); i++) {
-        STRING.push_back(s[i]);
-    }
-    sort(STRING.begin(), STRING.end(), cmp);
-    for(char ch :STRING) {
-        answer += ch;
-    }
-    return answer;
-}
